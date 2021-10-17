c21a2c08538e53685895885bee58a86a1b995384
hhummus <Helena_L93@hotmail.com>
2021-10-17 20:07:23 +0200

    corrected path files for images

--- js/news.js
+++ js/news.js
@@ -4,57 +4,56 @@ let cardBody = document.querySelector('.container-of-all-cards');
 // cards array
 const newsArray = [
 	{
-		newsImage: '/images/news/news-1.jpeg',
+		newsImage: '/images/news/news-1.jpg',
 		newsT: 'Nunc porttitor vel.',
 		newsP: 'Nunc malesuada eget est fringilla dapibus.',
 		newsMoreButton: 'More'
-    },
-    {
-		newsImage: '/images/news/news-2.jpeg',
+	},
+	{
+		newsImage: '/images/news/news-2.jpg',
 		newsT: 'Nunc porttitor vel.',
 		newsP: 'Nunc malesuada eget est fringilla dapibus.',
 		newsMoreButton: 'More'
-    },
-    {
-		newsImage: '/images/news/news-3.jpeg',
+	},
+	{
+		newsImage: '/images/news/news-3.jpg',
 		newsT: 'Nunc porttitor vel.',
 		newsP: 'Nunc malesuada eget est fringilla dapibus.',
 		newsMoreButton: 'More'
-    },
-    {
-		newsImage: '/images/news/news-4.jpeg',
+	},
+	{
+		newsImage: '/images/news/news-4.jpg',
 		newsT: 'Nunc porttitor vel.',
 		newsP: 'Nunc malesuada eget est fringilla dapibus.',
 		newsMoreButton: 'More'
-    },
-    {
-		newsImage: '/images/news/news-5.jpeg',
+	},
+	{
+		newsImage: '/images/news/news-5.jpg',
 		newsT: 'Nunc porttitor vel.',
 		newsP: 'Nunc malesuada eget est fringilla dapibus.',
 		newsMoreButton: 'More'
-    },
-    {
-		newsImage: '/images/news/news-6.jpeg',
+	},
+	{
+		newsImage: '/images/news/news-6.jpg',
 		newsT: 'Nunc porttitor vel.',
 		newsP: 'Nunc malesuada eget est fringilla dapibus.',
 		newsMoreButton: 'More'
-    },
-    {
-		newsImage: '/images/news/news-7.jpeg',
+	},
+	{
+		newsImage: '/images/news/news-7.jpg',
 		newsT: 'Nunc porttitor vel.',
 		newsP: 'Nunc malesuada eget est fringilla dapibus.',
 		newsMoreButton: 'More'
-    },
-    {
-		newsImage: '/images/news/news-8.jpeg',
+	},
+	{
+		newsImage: '/images/news/news-8.jpg',
 		newsT: 'Nunc porttitor vel.',
 		newsP: 'Nunc malesuada eget est fringilla dapibus.',
 		newsMoreButton: 'More'
-    },
+	}
 ];
 
-
-// forEach through array of cards and display in html 
+// forEach through array of cards and display in html
 newsArray.forEach(function(cardElements) {
 	cardBody.innerHTML += `
 							<div class='card__container col-sm-4'>
